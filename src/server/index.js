576bd306481bc11930535ffc4c99646efcf39c84
jsiilva1 <jsiilva@outlook.com.br>
2019-12-08 22:24:46 -0300

    Changed app port

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@
 import bootstrapEnvironment from '../lib/bootstrap';
 import app from './app';
 
-const { APP_PORT = 3000 } = process.env;
+const { APP_PORT = 5000 } = process.env;
 
 bootstrapEnvironment();
 const startServer = async app => app.listen(APP_PORT, () => console.log(`Server is running in port ${APP_PORT}`));
